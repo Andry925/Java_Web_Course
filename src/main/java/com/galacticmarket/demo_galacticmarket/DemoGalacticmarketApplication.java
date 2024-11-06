b843a5246a3975e41349c04a26ab9667e058a395
Andry925 <andry925@ukr.net>
2024-11-06 13:00:34 +0200

    chore: add initial spring project

---
+++ src/main/java/com/galacticmarket/demo_galacticmarket/DemoGalacticmarketApplication.java
@@ -0,0 +1,13 @@
+package com.galacticmarket.demo_galacticmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGalacticmarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGalacticmarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/galacticmarket/demo_galacticmarket/DemoGalacticmarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galacticmarket.demo_galacticmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGalacticmarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
