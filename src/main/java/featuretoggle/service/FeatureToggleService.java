1c6073b7cdcd156b37ebe862203fc9e1610fbd8f
Andry925 <andry925@ukr.net>
2024-12-02 21:22:05 +0200

    feat: add FeatureToggleService

--- src/main/java/featuretoggle/service/FeatureToggleService.java
+++ src/main/java/featuretoggle/service/FeatureToggleService.java
@@ -1,4 +1,21 @@
 package featuretoggle.service;
 
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class FeatureToggleService {
+
+    private final Environment environment;
+
+    public FeatureToggleService(Environment environment) {
+        this.environment = environment;
+    }
+
+    public boolean check(String featureName) {
+        return Optional.ofNullable(environment.getProperty("application.feature.toggles." + featureName, Boolean.class))
+                .orElse(false);
+    }
 }
