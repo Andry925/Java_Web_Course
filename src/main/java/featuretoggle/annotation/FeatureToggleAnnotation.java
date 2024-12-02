b44093c5a2a47f39ddb1bf45d0061246fec9bc91
Andry925 <andry925@ukr.net>
2024-12-02 21:13:40 +0200

    feat: implement FeatureToggleAnnotation

---
+++ src/main/java/featuretoggle/annotation/FeatureToggleAnnotation.java
@@ -0,0 +1,15 @@
+package featuretoggle.annotation;
+
+
+import featuretoggle.FeatureToggles;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.TYPE})
+public @interface FeatureToggleAnnotation {
+    FeatureToggles value();
+}
