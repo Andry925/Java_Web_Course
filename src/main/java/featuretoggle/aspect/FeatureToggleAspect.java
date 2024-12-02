a868da21e0384f46fa8f5d462b9d7764b98d0f0d
Andry925 <andry925@ukr.net>
2024-12-02 21:21:38 +0200

    feat: add FeatureToggleAspect

--- src/main/java/featuretoggle/aspect/FeatureToggleAspect.java
+++ src/main/java/featuretoggle/aspect/FeatureToggleAspect.java
@@ -1,4 +1,34 @@
 package featuretoggle.aspect;
 
+
+import featuretoggle.FeatureToggles;
+import featuretoggle.annotation.FeatureToggleAnnotation;
+import featuretoggle.exceptions.FeatureNotAvailableException;
+import featuretoggle.service.FeatureToggleService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Aspect
+@Component
+@RequiredArgsConstructor
 public class FeatureToggleAspect {
+
+    private final FeatureToggleService featureToggleService;
+
+    @Before("@annotation(featureToggleAnnotation)")
+    public void checkFeatureToggle(FeatureToggleAnnotation featureToggleAnnotation) {
+        FeatureToggles toggle = featureToggleAnnotation.value();
+        String featureName = toggle.getFeatureName();
+
+        if (!featureToggleService.check(featureName)) {
+            log.warn("Feature toggle '{}' is disabled. Access denied.", featureName);
+            throw new FeatureNotAvailableException(featureName);
+        }
+
+        log.info("Feature toggle '{}' is enabled. Proceeding with execution.", featureName);
+    }
 }
