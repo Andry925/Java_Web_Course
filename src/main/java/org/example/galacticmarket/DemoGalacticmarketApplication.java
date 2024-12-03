d7215d636e88c3ddbea7a2f8765ad9ced98af889
Andry925 <andry925@ukr.net>
2024-12-03 11:30:24 +0200

    chore: add dependencies to main app

--- src/main/java/org/example/galacticmarket/DemoGalacticmarketApplication.java
+++ src/main/java/org/example/galacticmarket/DemoGalacticmarketApplication.java
@@ -1,9 +1,15 @@
 package org.example.galacticmarket;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
-@SpringBootApplication
+
+@SpringBootApplication(scanBasePackages = {
+		"org.example.galacticmarket",
+		"featuretoggle.service",
+		"featuretoggle"
+})
+@EnableAspectJAutoProxy
 public class DemoGalacticmarketApplication {
 
 	public static void main(String[] args) {
